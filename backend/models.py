2b9917aa9be48ca70dbf50975861dbf5e69980e4
carlos <carlos.alvarez.s@usach.cl>
2021-03-10 13:56:15 -0300

    Se arregla conexion con django y base de datos

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Temperature(models.Model): 
+	#_id = models.ObjectIdField()
+	city = models.CharField(max_length=255)
+	temperature = models.FloatField()
+	timestamp = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
